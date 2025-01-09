9ed5eeed856477df788e3346377ac38dbe6fd247
Dharamraj <148431433+Dharamraj82@users.noreply.github.com>
2025-01-09 11:34:24 +0530

    Update Page1_Text.jsx

--- src/Components/Page1_Text.jsx
+++ src/Components/Page1_Text.jsx
@@ -9,7 +9,7 @@ const Page1_Text = () => {
     <div className='w-5/6 text-4xl sm:text-5xl lg:text-7xl sm:pt-[25vh] pt-[15vh] pl-[3vw]  text-zinc-50 uppercase font-bold flex flex-col'>
         <h1>I Am <span className={` ${dark ?'text-zinc-950':'text-[#2600ff]'}`}>Website</span></h1>
         <h1>Devloper</h1>
-        <h1>To Hire</h1>
+        <h1>Hire Me</h1>
     </div>
     </>
   )
